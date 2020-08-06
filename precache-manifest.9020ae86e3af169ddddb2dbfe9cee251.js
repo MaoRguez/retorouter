ad62b2a6a17eeae396e553b88d8e02eee9fd8393
Maorguez <avila.mauro@gmail.com>
2020-08-05 23:35:02 -0500

    Updates

---
+++ precache-manifest.9020ae86e3af169ddddb2dbfe9cee251.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a62fd2f7058edb6fa12d098ea83cf0d",
+    "url": "/retorouter/index.html"
+  },
+  {
+    "revision": "e1d1b9802c4d8c443be5",
+    "url": "/retorouter/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "1db3413eba169a626212",
+    "url": "/retorouter/static/css/main.84aabb4f.chunk.css"
+  },
+  {
+    "revision": "e1d1b9802c4d8c443be5",
+    "url": "/retorouter/static/js/2.ce0b91b7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/retorouter/static/js/2.ce0b91b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1db3413eba169a626212",
+    "url": "/retorouter/static/js/main.a1b11931.chunk.js"
+  },
+  {
+    "revision": "14142dbfdaf211fe6fa8",
+    "url": "/retorouter/static/js/runtime-main.5064f274.js"
+  },
+  {
+    "revision": "bacec8f18d4975fe1186c420f90f3559",
+    "url": "/retorouter/static/media/001-home.bacec8f1.svg"
+  },
+  {
+    "revision": "4a2c918df5f1939572bf6e13e5c94894",
+    "url": "/retorouter/static/media/115-users.4a2c918d.svg"
+  },
+  {
+    "revision": "49998e39b94c426adcc243b143013334",
+    "url": "/retorouter/static/media/388-mail.49998e39.svg"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/retorouter/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "a5ee2d16b15c476fe62cf9c142401b9e",
+    "url": "/retorouter/static/media/kisspng.a5ee2d16.png"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/retorouter/static/media/stars.1d17360a.svg"
+  }
+]);
